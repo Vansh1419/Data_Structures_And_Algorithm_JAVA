f9432e04326cb3ab24055236c1a65c68263d7381
Vansh1419 <vanshjain1419@gmail.com>
2022-08-12 16:54:21 +0530

    1.1.1 Base Types completed

---
+++ _1_Java_Programming_Basics/_1.1_Preliminaries/A_Base_Types.java
@@ -0,0 +1,39 @@
+/*
+ * public: This says anyone can run this program
+ * class: All code in a Java program must belong to a class
+ * A_Base_Types: The name of this class
+ * static: This method belong to the class, not an object (more on later)
+ * void: This method doesn't return anything
+ * main: Method where program begins
+ * String[] args: Parameters passed to this method
+ * System.out.println: The name of the method we want to call(in this case the method for printing strings on the screen)
+ * 
+ * Reserved Words: There are 33 reversed words in Java are:-
+ * abstract     default     goto            package         synchronized
+ * assert       do          if              private         this
+ * boolean      double      implements      protected       throw
+ * break        else        import          public          throws
+ * byte         enum        instanceof      return          transient
+ * case         extenda     int             short           true
+ * catch        final       long            scriptfp        void
+ * char         false       interface       static          try
+ * class        finally     native          super           volatile
+ * const        float       new             switch          while
+ * continue     for         null
+ * 
+ * Base Types: 
+ *      └── Boolean: true or false
+ *      └── char: 16-bit unicode character
+ *      └── byte: 8-bit signed two's complement integer
+ *      └── short: 16-bit signed two's complement integer
+ *      └── int: 32-bit signed two's complement integer
+ *      └── long: 64-bit signed two's complement integer
+ *      └── float: 32-bit floating-point number (IEEE 754-1985)
+ *      └── double: 64-bit floating-point number (IEEE 754-1985)
+ * 
+ */
+public class A_Base_Types {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
